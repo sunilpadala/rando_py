1ccdbdecd8a7c6dd7dc1d4aa830d1164cd805857
Sunil PC <sunil4frnz@gmail.com>
2020-12-08 00:06:24 +0530

    corrected test values

--- test_calc.py
+++ test_calc.py
@@ -9,7 +9,7 @@ class calc_test(unittest.TestCase):
 
     def test_sub(self):
         self.assertEqual(ci.sub(24,21),3)
-        self.assertEqual(ci.sub(24,21),34)
+        self.assertEqual(ci.sub(55,21),34)
 
 
 if __name__ == '__main__':
